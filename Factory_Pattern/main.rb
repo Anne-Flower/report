a2160863c498b35826254be4441ef978f69b30ca
Anne Flore Bernard <b.anneflore@yahoo.fr>
2024-12-26 21:04:28 +0100

    wip2

--- Factory_Pattern/main.rb
+++ Factory_Pattern/main.rb
@@ -1,2 +1,32 @@
-require_relative 'button'
-require_relative 'text_field'
+require_relative 'desktop_widget_factory'
+require_relative 'mobile_widget_factory'
+require_relative 'widget_factory'
+
+
+class Renderer
+  attr_writer :widget_factory
+
+  def initialize(factory)
+    @factory = factory
+  end
+
+  def render
+    button = @factory.create_button
+    text_field = @factory.create_textfield
+
+    puts button.render
+    puts text_field.render
+  end
+end
+
+desktop_factory = DesktopWidgetFactory.new
+desktop_renderer = Renderer.new(desktop_factory)
+
+puts "voila les widgets desktop:"
+desktop_renderer.render
+
+
+mobile_factory = MobileWidgetFactory.new
+mobile_renderer = Renderer.new(mobile_factory)
+
+mobile_renderer.render
--- Factory_Pattern/mobile_widget_factory.rb
+++ Factory_Pattern/mobile_widget_factory.rb
@@ -3,6 +3,7 @@ require_relative 'desktop_btn'
 require_relative 'text_field_desktop'
 #concret
 class MobileWidgetFactory < WidgetFactory
+  
   def create_button
     MobileBtn.new
   end
